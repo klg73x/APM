8f4041ce844a41fd7723a4673af89566b69f542b
Kevin Gagnon <klg73@outlook.com>
2018-03-25 10:52:32 -0400

    Retrieving Data using HTTP Competed

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -13,6 +13,7 @@ export class ProductListComponent implements OnInit {
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
+    errorMessage: string;
     _listFilter: string;
     get listFilter(): string {
         return this._listFilter;
@@ -36,8 +37,13 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.products = this._productService.getProducts();
-        this.filteredProducts = this.products;
+        this._productService.getProducts()
+            .subscribe(products => {
+                this.products = products;
+                this.filteredProducts = this.products;
+            },
+                error => this.errorMessage = <any>error);
+
     }
 
     onRatingClicked(message: string): void {
